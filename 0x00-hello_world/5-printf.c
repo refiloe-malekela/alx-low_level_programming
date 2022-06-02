766f6a82fbae36adb2e2c758a99393f35d03ce07
refiloe-malekela <refinedmalekela@gmail.com>
2022-06-02 10:43:48 -0700

    Other commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Write a C program that prints, followed by a new line.
+ * Return : Always 0 
+ */
+
+int main(void)
+{ 
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
