bb62867021569bfd2531cd580ccbc7250d245bba
refiloe-malekela <refinedmalekela@gmail.com>
2022-06-02 11:29:23 -0700

    Other commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
+#!/bin/bash
 #include <stdio.h>
 
 int main(void)
 {
 	//string initialisation
-	char Mystr[] =""Programming is like building a multilingual puzzle";
+	char Mystr[32] =""Programming is like building a multilingual puzzle";
 	puts(Mystr); //writing the string to stdout
 	return 0;
 }
