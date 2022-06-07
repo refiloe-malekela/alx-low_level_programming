218e7470bca3b692f170ac95d63fae95944f8554
refiloe-malekela <refinedmalekela@gmail.com>
2022-06-07 04:43:14 -0700

    Other commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void){
+	char i='a';
+	while(i<='z'){
+		_putchar(i);
+		i ++;
+	}
+		
+		_putchar('\n);
+}
