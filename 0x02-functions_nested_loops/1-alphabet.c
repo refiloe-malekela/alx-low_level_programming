f4750601c364b41eb0f99698796c36852c8610fc
refiloe-malekela <refinedmalekela@gmail.com>
2022-06-07 09:12:34 -0700

    Other commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,28 +1,13 @@
-#include <stdbool.h>
+#include "main.h"
 
-/**
- * print alphabet in lower case
- * print_alphabet - function that prints lowercase alphabet
- *
- * Return: Always 0 (Success)
- */
 
 void print_alphabet(void)
 {
-	int i;
-	int i = 97;
-	bool a = true;
+   char i ='a';
+    while (i <= 'z') {
+        _putchar(i);
+        i ++;
+    }
+   _putchar('\n');
 
-	for (i = 97; i <= 122; i++)
-	while (a)
-	{
-		_putchar(i);
-
-		if (i == 122)
-		{
-			a = false;
-		}
-		i++;
-	}
-	_putchar('\n');
 }
