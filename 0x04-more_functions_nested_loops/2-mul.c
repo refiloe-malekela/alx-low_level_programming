3da4ba7a1ac83cc176b5f3a72b84b008754516dd
refiloe-malekela <refinedmalekela@gmail.com>
2022-06-10 14:52:12 -0700

    Other commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul -> multiplies two integers.
+ *
+ * @a : integer parameter1
+ *
+ * @b : integer parameter2
+ *
+ * Return: return 0 on success and 1 on fail
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
